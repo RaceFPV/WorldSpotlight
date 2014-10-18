e1803cd82c4d20693b666ae2e6faa5aa2784be0d
Alexander Taylor <a@alexandertaylor.org>
2014-10-18 06:45:07 +0100

    Precompile fonts for heroku

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(*Rails.groups)
 module Worldspotlight
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
