84c96904016e32c4e20a766c70781a90d4c0a92c
slacker87 <racethesunlive@yahoo.com>
2014-10-18 05:22:26 +0000

    make weather work

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,6 +5,8 @@ class MapController < ApplicationController
   def show
     @country = params[:id]
     @countryname = Country.find_country_by_alpha2(@country)
+    client = YahooWeather::Client.new
+    @weather = client.fetch_by_location(@country)
     return render partial: 'show.js.erb'
   end
 
