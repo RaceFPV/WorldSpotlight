3491d8b7d602ab0344770c088e6bac1b8d4c8452
alexander taylor <a@alexandertaylor.org>
2014-10-19 03:54:32 +0000

    Weather icons (first iteration)

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -1,2 +1,16 @@
 module MapHelper
+  def get_weather_icon(temp)
+    case temp
+    when -500..0
+      image_tag "verycold.png", class: 'weather-icon'
+    when 1..31
+       image_tag "cold.png", class: 'weather-icon'
+    when 32..50
+       image_tag "cool.png", class: 'weather-icon'
+    when 51..86
+       image_tag "warm.png", class: 'weather-icon'
+    when 86..500
+       image_tag "hot.png", class: 'weather-icon'
+    end
+  end
 end
