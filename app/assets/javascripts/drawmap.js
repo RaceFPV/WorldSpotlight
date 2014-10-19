39f869b6d056f4a2dc51eb3f7a6a7341a58f4555
alexander taylor <a@alexandertaylor.org>
2014-10-19 17:40:46 +0000

    Add random button

--- app/assets/javascripts/drawmap.js
+++ app/assets/javascripts/drawmap.js
@@ -33,6 +33,19 @@ $(function(){
         var regions = selectedRegions;
         regionclick(regions);
     }});
+  
+  var country_codes = ["VA", "CC", "GT", "JP", "SE", "TZ", "CD", "GU", "MM", "DZ", "MN", "PK", "SG", "VC", "CF", "GW", "MO", "PL", "SH", "VE", "CG", "MP", "PM", "SI", "ZW", "CH", "GY", "MQ", "PN", "SJ", "VG", "CI", "MR", "SK", "MS", "SL", "YE", "VI", "CK", "ID", "MT", "SM", "CL", "IE", "LA", "MU", "SN", "CM", "FI", "LB", "MV", "PR", "SO", "CN", "FJ", "LC", "MW", "CO", "FK", "MX", "PT", "VN", "MY", "SR", "FM", "MZ", "CR", "PW", "FO", "ST", "IL", "LI", "PY", "BA", "CU", "SV", "CV", "FR", "IN", "LK", "VU", "BB", "IO", "CX", "RE", "UA", "SY", "CY", "IQ", "SZ", "BD", "CZ", "IR", "YT", "BE", "YU", "IS", "BF", "EC", "FX", "IT", "OM", "BG", "BH", "LR", "UG", "BI", "EE", "LS", "BJ", "LT", "EG", "EH", "LU", "RO", "UK", "BM", "LV", "BN", "UM", "BO", "KE", "NA", "LY", "BR", "KG", "NC", "BS", "HK", "KH", "BT", "KI", "NE", "QA", "RU", "US", "HM", "NF", "BV", "ER", "HN", "NG", "RW", "BW", "ES", "ET", "NI", "AD", "BY", "KM", "AE", "BZ", "HR", "KN", "TC", "AF", "NL", "TD", "UY", "AG", "HT", "KP", "UZ", "GA", "HU", "TF", "AI", "DE", "KR", "TG", "NO", "TH", "GD", "NP", "ZA", "WF", "AL", "GE", "TJ", "AM", "GF", "NR", "TK", "AN", "DJ", "KW", "AO", "DK", "TM", "GH", "MA", "KY", "NU", "TN", "DM", "GI", "KZ", "TO", "AQ", "MC", "TP", "AR", "MD", "AS", "DO", "PA", "TR", "AT", "GL", "NZ", "AU", "GM", "MG", "GN", "ZM", "MH", "TT", "AW", "PE", "SA", "GP", "WS", "PF", "SB", "TV", "CA", "GQ", "JM", "SC", "TW", "AZ", "GR", "MK", "PG", "SD", "GS", "JO", "ML", "PH"]
+ 
+    $('#random_button').on( "click", function() {
+      var selected = country_codes[Math.floor(Math.random() * country_codes.length)];
+      console.log(selected)
+      map.clearSelectedRegions();
+      map.setSelectedRegions(selected);
+      $(this).fadeOut();
+        setTimeout(function() {
+          $('#random_button').fadeIn();
+        },3000); 
+    });
 });
 function regionclick(regions){
         $.get( "map/" +regions);
